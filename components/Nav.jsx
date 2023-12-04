dbbfc893dd59414dc33e6a37ea862749dac6a89f
macr0dose <121298889+macr0dose@users.noreply.github.com>
2023-12-04 16:01:29 +1000

    update nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 
-import Link from 'next/link';
-import Image from 'next/image';
-import { useState, useEffect } from 'react';
-import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
+import Link from "next/link";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
   const { data: session } = useSession();
@@ -12,12 +12,10 @@ const Nav = () => {
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setUpProviders = async () => {
-      const response = await getProviders();
-      setProviders(response);
-    }
-
-    setUpProviders();
+    (async () => {
+      const res = await getProviders();
+      setProviders(res);
+    })();
   }, []);
 
   return (
@@ -25,16 +23,15 @@ const Nav = () => {
       <Link href='/' className='flex gap-2 flex-center'>
         <Image
           src='/assets/images/logo.svg'
-          alt='Promptopia logo'
+          alt='logo'
           width={30}
           height={30}
           className='object-contain'
-          loading="eager"
         />
         <p className='logo_text'>Promptopia</p>
       </Link>
 
-    {/*Desktop Navigation*/}
+      {/* Desktop Navigation */}
       <div className='sm:flex hidden'>
         {session?.user ? (
           <div className='flex gap-3 md:gap-5'>
@@ -68,14 +65,14 @@ const Nav = () => {
                   }}
                   className='black_btn'
                 >
-                  Sign In
+                  Sign in
                 </button>
               ))}
           </>
         )}
       </div>
 
-      {/* Mobile navigation*/}
+      {/* Mobile Navigation */}
       <div className='sm:hidden flex relative'>
         {session?.user ? (
           <div className='flex'>
@@ -85,7 +82,7 @@ const Nav = () => {
               height={37}
               className='rounded-full'
               alt='profile'
-              onClick={() => setToggleDropdown((prev) => !prev)}
+              onClick={() => setToggleDropdown(!toggleDropdown)}
             />
 
             {toggleDropdown && (
@@ -106,14 +103,14 @@ const Nav = () => {
                 </Link>
                 <button
                   type='button'
-                  onClick={() =>  {
+                  onClick={() => {
                     setToggleDropdown(false);
                     signOut();
                   }}
                   className='mt-5 w-full black_btn'
                 >
                   Sign Out
-                  </button>
+                </button>
               </div>
             )}
           </div>
@@ -124,11 +121,12 @@ const Nav = () => {
                 <button
                   type='button'
                   key={provider.name}
-                  onClick={() => {signIn(provider.id);
+                  onClick={() => {
+                    signIn(provider.id);
                   }}
                   className='black_btn'
                 >
-                  Sign In
+                  Sign in
                 </button>
               ))}
           </>
@@ -137,6 +135,5 @@ const Nav = () => {
     </nav>
   );
 };
-
 
 export default Nav;
