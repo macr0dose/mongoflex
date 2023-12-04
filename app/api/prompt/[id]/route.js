ab6f3c25b6a3039eee3a3abc4c99443961e2f220
macr0dose <121298889+macr0dose@users.noreply.github.com>
2023-12-04 21:36:55 +1000

    update feed

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -1,29 +1,15 @@
 import { connectToDB } from '@utils/database';
 import Prompt from '@models/prompt';
-import User from "@models/user";
 
 //GET (read)
 export const GET = async (request, { params }) => {
     try {
-
         await connectToDB();
-        const prompts = await Prompt.find().populate({
-          path: "creator"
-        });
 
-        const response = new Response(JSON.stringify(prompts), {
-          status: 200,
-        });
+        const prompt = await Prompt.findById(params.id).populate('creator')
+        if(!prompt) return new Response('Prompt not found', { status: 404 });
 
-        // Add a unique identifier to the URL to force a cache-busting reload
-        const url = new URL(request.url);
-        url.searchParams.set("t", Date.now());
-        response.headers.set("Cache-Control", "no-cache, no-store, must-revalidate");
-        response.headers.set("Pragma", "no-cache");
-        response.headers.set("Expires", "0");
-        response.headers.set("Location", url.toString());
-
-        return response;
+        return new Response(JSON.stringify(prompt), { status: 200 })
         } catch (error) {
             return new Response('Failed to fetch all prompts', { status: 500 })
     }
