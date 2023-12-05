67c1cc7380e7e1ed960fa1c9d5e6ac58e67a81d5
macr0dose <121298889+macr0dose@users.noreply.github.com>
2023-12-05 15:47:16 +1000

    testing new route js

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -6,14 +6,13 @@ export const GET = async (request, { params }) => {
     try {
         await connectToDB();
 
-        const prompt = await Prompt.findById(params.id).populate('creator')
-        if(!prompt) return new Response('Prompt not found', { status: 404 });
-
-        return new Response(JSON.stringify(prompt), { status: 200 })
-        } catch (error) {
-            return new Response('Failed to fetch all prompts', { status: 500 })
+        const prompts = await Prompt.find({}).populate('creator');
+        return new Response(JSON.stringify(prompts), { status: 200 });
+    } catch (error) {
+        console.error("Error in GET /api/prompt:", error); // Log the error for debugging
+        return new Response(`Failed to fetch all prompts: ${error.message}`, { status: 500 });
     }
-}
+};
 
 //PATCH
 export const PATCH = async (request, { params }) => {
