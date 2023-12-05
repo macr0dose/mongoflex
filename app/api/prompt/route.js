f1ff60ea53e75f85096f52e85b4a05cf3b54ed14
macr0dose <121298889+macr0dose@users.noreply.github.com>
2023-12-05 11:26:05 +1000

    update prompt route

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,6 +1,8 @@
 import { connectToDB } from '@utils/database';
 import Prompt from '@models/prompt';
 
+export const revalidate = 0;
+
 export const GET = async (request, { params }) => {
     try {
         await connectToDB();
