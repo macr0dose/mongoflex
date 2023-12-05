4b1536c14cad967024192bd23118f2c2318c4f0c
macr0dose <121298889+macr0dose@users.noreply.github.com>
2023-12-05 15:49:46 +1000

    testing new route js

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -6,13 +6,14 @@ export const GET = async (request, { params }) => {
     try {
         await connectToDB();
 
-        const prompts = await Prompt.find({}).populate('creator');
-        return new Response(JSON.stringify(prompts), { status: 200 });
-    } catch (error) {
-        console.error("Error in GET /api/prompt:", error); // Log the error for debugging
-        return new Response(`Failed to fetch all prompts: ${error.message}`, { status: 500 });
+        const prompt = await Prompt.findById(params.id).populate('creator')
+        if(!prompt) return new Response('Prompt not found', { status: 404 });
+
+        return new Response(JSON.stringify(prompt), { status: 200 })
+        } catch (error) {
+            return new Response('Failed to fetch all prompts', { status: 500 })
     }
-};
+}
 
 //PATCH
 export const PATCH = async (request, { params }) => {
--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -8,9 +8,9 @@ export const GET = async (request, { params }) => {
         await connectToDB();
 
         const prompts = await Prompt.find({}).populate('creator');
-
-        return new Response(JSON.stringify(prompts), { status: 200 })
-        } catch (error) {
-            return new Response('Failed to fetch all prompts', { status: 500 })
+        return new Response(JSON.stringify(prompts), { status: 200 });
+    } catch (error) {
+        console.error("Error in GET /api/prompt:", error); // Log the error for debugging
+        return new Response(`Failed to fetch all prompts: ${error.message}`, { status: 500 });
     }
-}
+};
