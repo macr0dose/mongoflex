5fcaee8c865b81d7a42eba32d380f720af050bf6
macr0dose <121298889+macr0dose@users.noreply.github.com>
2023-12-11 18:56:54 +1000

    first commit

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,7 +1,7 @@
 "use client"
 import { useState } from 'react';
 import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/router'; // Corrected import path
+import { useRouter } from 'next/navigation'; // Corrected import path
 
 import Form from '@components/Form';
 
