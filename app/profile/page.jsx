647dc58d8496e13b1b282be2059a209f1b6a7829
macr0dose <121298889+macr0dose@users.noreply.github.com>
2023-12-11 19:08:45 +1000

    first commit

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,67 +1,58 @@
-"use client"
-import { useState } from 'react';
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation'; // Corrected import path
+"use client"; // Ensures the component is treated as a Client Component
 
-import Form from '@components/Form';
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router"; // Corrected import path
 
-const CreatePrompt = () => {
+import Profile from "@components/Profile";
+
+const MyProfile = () => {
   const router = useRouter();
   const { data: session } = useSession();
-  
-  const [submitting, setSubmitting] = useState(false);
-  const [post, setPost] = useState({
-    title: '',
-    description: '',
-    image: '',
-    liveSiteUrl: '',
-    githubUrl: '',
-    category: '',
-  });
 
-  const createPrompt = async (e) => {
-    e.preventDefault();
-    setSubmitting(true);
+  const [myPrompts, setMyPrompts] = useState([]);
 
-    const formData = new FormData();
-    formData.append('title', post.title);
-    formData.append('description', post.description);
-    formData.append('image', post.image);
-    formData.append('liveSiteUrl', post.liveSiteUrl);
-    formData.append('githubUrl', post.githubUrl);
-    formData.append('category', post.category);
-    formData.append('createdBy', session?.user.id); // Assuming session.user.id holds the User ID
+  useEffect(() => {
+    const fetchPrompts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/prompts`);
+      const data = await response.json();
 
-    try {
-      const response = await fetch('/api/prompt/new', {
-        method: 'POST',
-        body: formData, // Sending as formData to handle the image file
-      });
+      setMyPrompts(data);
+    };
 
-      if (response.ok) {
-        router.push('/');
+    if (session?.user.id) fetchPrompts();
+  }, [session?.user.id]);
+
+  const handleEdit = (prompt) => {
+    router.push(`/update-prompt?id=${prompt._id}`);
+  };
+
+  const handleDelete = async (prompt) => {
+    const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${prompt._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPrompts = myPrompts.filter((item) => item._id !== prompt._id);
+        setMyPrompts(filteredPrompts);
+      } catch (error) {
+        console.error(error);
       }
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setSubmitting(false);
     }
   };
 
-  const handleImageUpload = (e) => {
-    setPost({ ...post, image: e.target.files[0] });
-  };
-
   return (
-    <Form
-      type="Create"
-      post={post}
-      setPost={setPost}
-      submitting={submitting}
-      handleSubmit={createPrompt}
-      handleImageUpload={handleImageUpload}
+    <Profile
+      name="My"
+      desc="Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination"
+      data={myPrompts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
     />
   );
 };
 
-export default CreatePrompt;
+export default MyProfile;
