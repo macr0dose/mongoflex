f37dc5caf4a7a415db8bfc595a488d356bcb43f5
macr0dose <121298889+macr0dose@users.noreply.github.com>
2023-12-11 16:18:22 +1000

    first commit

--- models/prompt.js
+++ models/prompt.js
@@ -1,20 +1,33 @@
 import { Schema, model, models } from 'mongoose';
 
-const PromptSchema = new Schema({
-    creator: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-    },
-    prompt: {
-        type: String,
-        required: [true, 'Prompt is required'],
-    },
-    tag: {
+const ProjectSchema = new Schema({
+  title: {
     type: String,
-    required: [true, 'Tag is required'],
-    }
+    required: [true, 'Title is required!'],
+    minlength: [3, 'Title should be at least 3 characters long']
+  },
+  description: {
+    type: String,
+    required: [true, 'Description is required!']
+  },
+  image: {
+    type: String,
+    required: [true, 'Image URL is required!']
+  },
+  liveSiteUrl: String,
+  githubUrl: String,
+  category: {
+    type: String,
+    required: [true, 'Category is required!']
+  },
+  createdBy: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'Creator is required!']
+  }
 });
 
-const Prompt = models.Prompt || model('Prompt', PromptSchema);
+// If a Project model has already been registered, use that, otherwise create a new one
+const Project = models.Project || model("Project", ProjectSchema);
 
-export default Prompt;
+export default Project;
--- models/user.js
+++ models/user.js
@@ -1,26 +1,42 @@
 import { Schema, model, models } from 'mongoose';
 
 const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required!'],
+    minlength: [2, 'Name should be at least 2 characters long'],
+    maxlength: [100, 'Name should not exceed 100 characters']
+  },
   email: {
     type: String,
     unique: [true, 'Email already exists!'],
     required: [true, 'Email is required!'],
   },
-  username: {
-    type: String,
-    required: [true, 'Username is required!'],
-    match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[\w.]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
+  avatarUrl: {
+    type: String, // URL validation can be added separately if needed
   },
-  image: {
+  description: {
     type: String,
-  }
+    minlength: [2, 'Description should be at least 2 characters long'],
+    maxlength: [1000, 'Description should not exceed 1000 characters'],
+    default: ''
+  },
+  githubUrl: {
+    type: String, // URL validation can be added separately if needed
+    default: ''
+  },
+  linkedInUrl: {
+    type: String, // URL validation can be added separately if needed
+    default: ''
+  },
+  // Assuming 'projects' is an array of Project IDs
+  projects: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Project'
+  }]
 });
 
-
-// models object is provided by Mongoose and stores all registered models
-//if a User is not registered, it will be created automatically
-//the new model is assigned to the User
-
+// If a User model has already been registered, use that, otherwise create a new one
 const User = models.User || model("User", UserSchema);
 
 export default User;
